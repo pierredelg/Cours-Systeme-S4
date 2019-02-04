383846c63f243c688071ed0626c9fbf0977afb54
Pierre DELGRANGE <delgranp@bouleau12.iut-infobio.priv.univ-lille1.fr>
2019-02-04 16:21:25 +0100

    ajout methode creer_serveur

--- webserver/main.c
+++ webserver/main.c
@@ -2,11 +2,30 @@
 # include <string.h>
 int main ( int argc , char ** argv )
 {
+
+
 /* Arnold Robbins in the LJ of February ’95 , describing RCS */
+
+/*
 if ( argc > 1 && strcmp ( argv [1] , "-advice" ) == 0) {
 printf ( " Don ’t Panic !\n " );
 return 42;
 }
 printf ( " Need an advice ?\n " );
 return 0;
+}
+*/
+
+
+int fd;
+char buffer[128];
+ssize_t ret;
+
+
+creer_serveur(8080);
+
+fd = open(,O_RONLY);
+
+if(fd == -1 ){
+	fprintf(stderr,"Erreur d'ouverture");
 }
--- webserver/socket.c
+++ webserver/socket.c
@@ -8,24 +8,31 @@ int creer_serveur(int port){
 	{
 		/* traitement de l ’ erreur */
 		perror ( "socket_serveur" );
+		return -1;
 	}
 	/* Utilisation de la socket serveur */
 
 	struct sockaddr_in saddr ;
 	saddr . sin_family = AF_INET ; /* Socket ipv4 */
-	saddr . sin_port = htons (8080); /* Port d ’é coute */
-	saddr . sin_addr . s_addr = INADDR_ANY ; /* é coute sur toutes les interfaces */
+	saddr . sin_port = htons (port); /* Port d ’écoute */
+	saddr . sin_addr . s_addr = INADDR_ANY ; /* écoute sur toutes les interfaces */
 
 	if ( bind ( socket_serveur , ( struct sockaddr *)& saddr , sizeof ( saddr )) == -1)
 	{
-		perror ( " bind socker_serveur " );
 		/* traitement de l ’ erreur */
+		perror ( "bind socker_serveur" );
+		return -1;
 	}
+
+	return socket_serveur;
+}
+
+int listen(){
 
 	if ( listen ( socket_serveur , 10) == -1)
-	{
-		perror ( " listen socket_serveur " );
-		/* traitement d ’ erreur */
+		{
+			/* traitement d ’ erreur */
+			perror ( "listen socket_serveur" );
+			return -1;
 	}
-
 }
